f9e37901162d9bed1a1f32ee551c1195d6507b23
tara97 <taraprasad9090@gmail.com>
2023-06-25 19:33:26 +0530

    Using hhtp header

--- spring-graphql-basics/graphql-java/src/main/java/com/example/graphqljava/controller/BookController.java
+++ spring-graphql-basics/graphql-java/src/main/java/com/example/graphqljava/controller/BookController.java
@@ -3,6 +3,11 @@ package com.example.graphqljava.controller;
 import com.example.graphqljava.domain.Author;
 import com.example.graphqljava.domain.Book;
 import com.example.graphqljava.service.BookService;
+import graphql.GraphQLContext;
+import graphql.schema.DataFetchingEnvironment;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
@@ -10,19 +15,25 @@ import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
+
 @Controller
 public class BookController {
 
     @Autowired
     BookService bookService;
 
+    @Autowired
+    HttpServletRequest httpServletRequest;
+
     @QueryMapping
-    public Book bookById(@Argument String id) {
+    public Book bookById(@Argument String id, DataFetchingEnvironment env, GraphQLContext context) {
         return bookService.findBookById(id);
     }
 
     @MutationMapping
     public Book updateBookPrice(@Argument String id, @Argument int price) {
+        String currency = httpServletRequest.getHeader("currency");
+        if(currency.equalsIgnoreCase("euro")) return bookService.updatePriceOfBook(id, price * 2);
         return bookService.updatePriceOfBook(id, price);
     }
 
